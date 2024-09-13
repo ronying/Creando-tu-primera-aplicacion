ef2c7d20c11d9488ae0039cc613318da6bfe332a
Rony Castejon <60992883+ronying@users.noreply.github.com>
2024-09-13 15:50:01 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido a Screen Match");
-        System.out.println("Pelicula: Matrix");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Película: Matrix");
 
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEbElPlan = true;
-        double notaDeLaPelicula =  8.2;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
 
-        double media = (8.2 + 6.0 + 9.0 ) / 3;
+        double media = (8.2 + 6.0 + 9.0) / 3;
         System.out.println(media);
 
         String sinopsis = """
-                Matrix es una paradoja
-                La mejor pelicula del fin del milenio
-                Fue lanzada en:
-                """ + fechaDeLanzamiento;
-
+               Matrix es una paradoja
+               La mejor película del fin del milenio
+               Fué lanzada en: 
+               """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
 
         int clasificacion = (int) (media / 2);
-        
         System.out.println(clasificacion);
     }
 }
