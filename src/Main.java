bbe5862d3ea0f27331545397499dc6996851fd75
Rony Castejon <60992883+ronying@users.noreply.github.com>
2024-09-13 14:59:57 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+    }
+}
