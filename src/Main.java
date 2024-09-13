f08e05aada4c0f86a4aedef82e973da3a9ec2d57
Rony Castejon <60992883+ronying@users.noreply.github.com>
2024-09-13 15:38:56 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,17 @@ public class Main {
 
         double media = (8.2 + 6.0 + 9.0 ) / 3;
         System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja
+                La mejor pelicula del fin del milenio
+                Fue lanzada en:
+                """ + fechaDeLanzamiento;
+
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media / 2);
+        
+        System.out.println(clasificacion);
     }
 }
