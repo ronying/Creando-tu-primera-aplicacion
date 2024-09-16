cdbbdb2c85b1fb2cba5682914a3c4a92baff1aa5
Rony Castejon <60992883+ronying@users.noreply.github.com>
2024-09-16 16:37:25 -0600

    Update
    Continuacion del curso

---
+++ src/Decisiones.java
@@ -0,0 +1,20 @@
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = false;
+        double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
+
+        if (fechaDeLanzamiento >= 2022){
+            System.out.println("Películas más populares");
+        }else {
+            System.out.println("Película Retro que aún vale la pena ver");
+        }
+
+        if (incluidoEnElPlan || tipoPlan.equals("plus")){
+            System.out.println("Disfrute de su película");
+        } else {
+            System.out.println("Película no disponible para su plan actual");
+        }
+    }
+}
